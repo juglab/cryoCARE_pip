75ab6ac06acb0918062717846e0e6766defce46e
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2022-02-18 16:33:53 +0100

    make cryoCARE more portable

--- cryocare/scripts/cryoCARE_predict.py
+++ cryocare/scripts/cryoCARE_predict.py
@@ -11,18 +11,7 @@ from cryocare.internals.CryoCAREDataModule import CryoCARE_DataModule
 
 import psutil
 
-
-def main():
-    parser = argparse.ArgumentParser(description='Run cryoCARE prediction.')
-    parser.add_argument('--conf')
-
-    args = parser.parse_args()
-    with open(args.conf, 'r') as f:
-        config = json.load(f)
-
-    dm = CryoCARE_DataModule()
-    dm.load(config['path'])
-
+def denoise(config: dict, mean: float, std: float):
     model = CryoCARE(None, config['model_name'], basedir=config['path'])
 
     even = mrcfile.mmap(config['even'], mode='r', permissive=True)
@@ -34,7 +23,8 @@ def main():
     odd.data.shape += (1,)
     denoised.data.shape += (1,)
 
-    mean, std = dm.train_dataset.mean, dm.train_dataset.std
+    # mean = 0.46514022
+    # std = 0.373164
 
     model.predict(even.data, odd.data, denoised.data, axes='ZYXC', normalizer=None, mean=mean, std=std,
                   n_tiles=config['n_tiles'] + [1, ])
@@ -50,6 +40,38 @@ def main():
         else:
             denoised.header[l] = even.header[l]
     denoised.header['mode'] = 2
+
+def main():
+    parser = argparse.ArgumentParser(description='Run cryoCARE prediction.')
+    parser.add_argument('--conf')
+
+    args = parser.parse_args()
+    with open(args.conf, 'r') as f:
+        config = json.load(f)
+
+    import os
+    import tarfile
+    import tempfile
+    if os.path.isfile(config['path']):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            tar = tarfile.open(config['path'], "r:gz")
+            tar.extractall(tmpdirname)
+            tar.close()
+            config['model_name'] = os.listdir(tmpdirname)[0]
+            config['path'] = os.path.join(tmpdirname)
+            # Opening JSON file
+            with open(os.path.join(tmpdirname,config['model_name'],"norm.json")) as f:
+                norm_data = json.load(f)
+                mean = norm_data["mean"]
+                std = norm_data["std"]
+            denoise(config, mean, std)
+    else:
+        dm = CryoCARE_DataModule()
+        dm.load(config['path'])
+        mean, std = dm.train_dataset.mean, dm.train_dataset.std
+        denoise(config,mean,std)
+
+
 
 
 if __name__ == "__main__":
